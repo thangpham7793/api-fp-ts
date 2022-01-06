e44c5976972f59779b1816087d189e9a4d3eaded
Thang <thangnus@gmail.com>
2022-01-06 20:37:49 +1300

    feat: set up express

--- src/either.ts
+++ src/either.ts
@@ -1,11 +1,34 @@
 import * as E from 'fp-ts/lib/Either'
 import { flow } from 'fp-ts/lib/function'
+import * as t from 'io-ts'
 
 type User = {
   name: string
 }
 
-type HttpResponse<T = Error> =
+const queryDecoder = new t.Type<string, string, unknown>(
+  'string',
+  (i: unknown): i is string => typeof i === 'string',
+  (input, context) =>
+    typeof input === 'string'
+      ? t.success(input)
+      : t.failure(input, context, 'name must be a non-empty string'),
+  t.identity,
+)
+
+const UserDatabase: User[] = [
+  {
+    name: 'John',
+  },
+  {
+    name: 'Smith',
+  },
+  {
+    name: 'Tom',
+  },
+]
+
+type HttpResponse<T = never> =
   | {
       type: 'Error'
       error: string
@@ -15,16 +38,25 @@ type HttpResponse<T = Error> =
       data: T
     }
 
-const getUserService = (name: unknown): E.Either<Error, User> => {
-  if (!name || typeof name !== 'string')
-    return E.left(new Error('No name is given'))
-  return E.right({ name })
+const getUserService = (name: string): E.Either<Error, User> => {
+  const maybeUser = UserDatabase.find((u) => u.name === name)
+  if (!maybeUser) return E.left(new Error('User not found'))
+
+  return E.right(maybeUser)
 }
 
-const handleError = (err: Error): HttpResponse => {
+const handleServiceError = (err: Error): HttpResponse => {
   return {
     type: 'Error',
     error: err.message,
+  }
+}
+
+const handleInputValidationError = <T>(errs: t.Errors): HttpResponse<T> => {
+  console.log(errs[0])
+  return {
+    type: 'Error',
+    error: errs.map((e) => e.message).join('\n'),
   }
 }
 
@@ -32,21 +64,31 @@ const handleSuccess = (u: User): HttpResponse<User> => {
   return { type: 'Success', data: u }
 }
 
+const tryGetUser = flow(
+  getUserService,
+  // map to api response
+  E.bimap(handleServiceError, handleSuccess),
+  // serialization
+  // E.bimap(JSON.stringify, JSON.stringify),
+  // // client acknowledges - delegate to whatever api framework (IO here)
+  // E.bimap(console.error, console.log),
+)
+
+// E.fold actually makes sense since it all ends up being a HttpResponse at the end of the day
+// it captures the data transformation in both cases
+
 // ramda's pipe but with type!
 const runGetUser = flow(
-  // business layer
-  getUserService,
-  // web layer
-  // map to api response
-  E.bimap(handleError, handleSuccess),
-  // serialization
-  E.bimap(JSON.stringify, JSON.stringify),
-  // client acknowledges - delegate to whatever api framework (IO here)
-  E.bimap(console.error, console.log),
+  queryDecoder.decode,
+  E.fold(<never>handleInputValidationError, tryGetUser),
 )
 
 const program = (input: unknown) => {
-  runGetUser(input)
+  returnToClient(runGetUser(input))
+}
+
+const returnToClient = (res: unknown) => {
+  console.log(`Received ${JSON.stringify(res)}`)
 }
 
 program('Tom')
